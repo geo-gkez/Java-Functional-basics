c304c1e147b50a07adf57e9028909b8fab630f66
geo-gkez <georgegezeris@gmail.com>
2021-11-28 16:35:41 +0200

    callbacks && lambdas

---
+++ src/main/java/finalsection/Callbacks.java
@@ -0,0 +1,23 @@
+package finalsection;
+
+import java.util.function.Consumer;
+
+public class Callbacks {
+    public static void main(String[] args) {
+
+        hello("Tonia",null,value ->{
+            System.out.println("no lastName provided for "+value);
+        });
+
+    }
+
+    static void hello(String firstName, String lastName, Consumer<String> callback){
+        System.out.println(firstName);
+        if(lastName != null){
+            System.out.println(lastName);
+        } else {
+            callback.accept(firstName);
+        }
+    }
+
+}
